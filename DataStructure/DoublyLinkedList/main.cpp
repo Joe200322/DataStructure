8c5ff83ca520c03686c996bb21336a4aef52e403
unknown <joerefaat22@gmail.com>
2023-04-08 20:55:29 +0200

    change the swap implementation in doubly linked list to make it swap the whole node not the data

--- DataStructure/DoublyLinkedList/main.cpp
+++ DataStructure/DoublyLinkedList/main.cpp
@@ -8,12 +8,10 @@ int main()
     a.insertAtTail(2);
     a.insertAtTail(3);
     a.insertAtTail(4);
+    a.insertAtTail(5);
     a.insertAtHead(0);
-    a.insertAt(66, 1);
-    a.removeAt(1);
-    a.replaceAt(100, 1);
     a.forwardTraversal();
-    a.swap(1, 2);
+    a.swap(2, 4);
     cout << "---------------------\n";
     a.forwardTraversal();
     cout << "---------------------\n";
